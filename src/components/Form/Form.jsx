d7e63a2afd4c1671cf21d31805a2a034567809ca
Kubatbekkk <kubineccastro@gmail.com>
2023-05-30 15:36:43 +0600

    refactor: separateDatePickerComponent

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,25 +1,16 @@
 import React, { Fragment } from 'react';
-import Modal from '../Modal/Modal';
 import './style.css';
 import {
   FormControl,
   FormLabel,
-  FormControlLabel,
   TextField,
   Button,
-  RadioGroup,
-  Radio,
   Typography,
   Divider,
   Box,
 } from '@mui/material';
 
 import { useForm, Controller, useFieldArray } from 'react-hook-form';
-
-// ? datepicker imports
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import NameInput from './FormComponents/NameInput';
 import PasswordInput from './FormComponents/PasswordInput';
 import EmailInput from './FormComponents/EmailInput';
@@ -27,6 +18,9 @@ import SelectInput from './FormComponents/SelectInput';
 import CheckboxInput from './FormComponents/CheckboxInput';
 import TextareaInput from './FormComponents/TextareaInput';
 import RadioInput from './FormComponents/RadioInput';
+import Modal from '../Modal/Modal';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers';
+import DatePickerComponent from './FormComponents/DatePickerComponent';
 
 const Form = () => {
   const [open, setOpen] = React.useState(false);
@@ -90,37 +84,7 @@ const Form = () => {
 
         <Box display="flex" alignItems="center" justifyContent="space-between">
           <RadioInput errors={errors} control={control} />
-
-          <FormControl margin="normal">
-            {/* <FormLabel sx={{ marginBottom: '5px' }}>Select your date</FormLabel> */}
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Controller
-                control={control}
-                name="dateOfFlight"
-                rules={{
-                  required: 'Please select date of flight',
-                }}
-                render={({ field }) => (
-                  <DatePicker
-                    {...field}
-                    value={field.value}
-                    onChange={(date) => field.onChange(date)}
-                    label="Select Date of Flight"
-                    slotProps={{
-                      textField: {
-                        onBlur: field.onBlur,
-                        variant: 'filled',
-                        error: !!errors.dateOfFlight,
-                        helperText: errors.dateOfFlight?.message,
-                      },
-                    }}
-                    inputFormat="MM/dd/yyyy"
-                    disablePast
-                  />
-                )}
-              />
-            </LocalizationProvider>
-          </FormControl>
+          <DatePickerComponent errors={errors} control={control} />
         </Box>
 
         <FormControl margin="normal" fullWidth>
---
+++ src/components/Form/FormComponents/DatePickerComponent.jsx
@@ -0,0 +1,42 @@
+import { FormControl } from '@mui/material';
+import React from 'react';
+import { Controller } from 'react-hook-form';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+const DatePickerComponent = ({ control, errors }) => {
+  return (
+    <FormControl margin="normal">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Controller
+          control={control}
+          name="dateOfFlight"
+          rules={{
+            required: 'Please select date of flight',
+          }}
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              value={field.value}
+              onChange={(date) => field.onChange(date)}
+              label="Select Date of Flight"
+              slotProps={{
+                textField: {
+                  onBlur: field.onBlur,
+                  variant: 'filled',
+                  error: !!errors.dateOfFlight,
+                  helperText: errors.dateOfFlight?.message,
+                },
+              }}
+              inputFormat="MM/dd/yyyy"
+              disablePast
+            />
+          )}
+        />
+      </LocalizationProvider>
+    </FormControl>
+  );
+};
+
+export default DatePickerComponent;
